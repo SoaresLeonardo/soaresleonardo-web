4e0409e03c16038f10966424c2a1eaa2f2137d27
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-26 15:15:03 -0300

    Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import {
+  Header,
+  HeaderContainer,
+  HeaderLinks,
+  Link,
+  Menu,
+  Title,
+  TitleContainer,
+} from "./HeaderStyled";
+
+import { FaTimes, FaBars } from "react-icons/fa";
+
+export default function HeaderPage() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSiderbar = () => {
+    setSidebar(!sidebar);
+  };
+
+  return (
+    <Header>
+      <HeaderContainer>
+        <TitleContainer>
+          <Title>Leonardo</Title>
+        </TitleContainer>
+        <Menu>
+          {sidebar ? (
+            <FaTimes onClick={showSiderbar} />
+          ) : (
+            <FaBars onClick={showSiderbar} />
+          )}
+        </Menu>
+        <HeaderLinks sidebar={sidebar}>
+          <Link>
+            <a href="#home">Home</a>
+          </Link>
+          <Link>
+            <a href="#about">Sobre mim</a>
+          </Link>
+          <Link>
+            <a href="#projects">Projetos</a>
+          </Link>
+          <Link>
+            <a href="#contact">Contato</a>
+          </Link>
+        </HeaderLinks>
+      </HeaderContainer>
+    </Header>
+  );
+}
