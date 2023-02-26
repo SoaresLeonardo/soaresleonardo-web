2d39c1dba95b8a6bb0c06566bbb13adf33a5f91f
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-26 16:04:46 -0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import { ButtonElement } from "../components/ButtonElement";
+import {
+  Description,
+  Home,
+  HomeActions,
+  HomeContainer,
+  HomeImage,
+  HomeIntro,
+  Subtitle,
+  Title,
+} from "../styles/HomeStyled";
+
+export default function HomePage() {
   return (
-    <>
-      <h1>Meu home</h1>
-    </>
+    <Home>
+      <HomeContainer>
+        <HomeIntro>
+          <Subtitle>Olá,</Subtitle>
+          <Title>Eu sou Leonardo.</Title>
+          <Description>Desenvolvedor front-end</Description>
+          <HomeActions>
+            <ButtonElement>Meus projetos</ButtonElement>
+          </HomeActions>
+        </HomeIntro>
+        <HomeImage>
+          <img src="../../public/foto.jpg" alt="Home image" />
+        </HomeImage>
+      </HomeContainer>
+    </Home>
   );
 }
