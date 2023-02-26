2281ac78cb804fc1b9c02a336c83f1efad2719e0
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-02-26 18:53:45 -0300

    add Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { AboutPage } from "./components/About";
 import HeaderPage from "./components/Header";
+import Projects from "./components/Projects";
 import HomePage from "./pages";
 
 export default function App() {
   return (
     <div>
-      <HeaderPage/>
-      <HomePage/>
-      <AboutPage/>
+      <HeaderPage />
+      <HomePage />
+      <AboutPage />
+      <Projects />
     </div>
   );
 }
